1e8f937a0f938aaee273264c034fd711ba73a7e6
sunnavy <sunnavy@bestpractical.com>
2010-04-15 06:46:37 +0800

    LWP and HTTP::Lite handlers

---
+++ lib/Plient/Handler/HTTPLite.pm
@@ -0,0 +1,62 @@
+package Plient::Handler::HTTPLite;
+use strict;
+use warnings;
+
+my ( $HTTPLite, %protocol, %method );
+sub method { $method{ $_[-1] } } # in case people call with ->
+
+
+sub init {
+    return unless $HTTPLite && $HTTPLite_config;
+    eval { require 'HTTP::Lite' } or return;
+    @protocol{HTTP}   = ();
+    $method{http_get} = sub {
+        my ( $uri, $args ) = @_;
+
+        my $http  = HTTP::Lite->new;
+        my $res = $http->request($uri) || '';
+        if ( $res == 200 || $res == 301 || $res == 302 ) {
+
+            # XXX TODO handle redirect
+            return $http->body;
+        }
+        else {
+            warn "failed to get $uri with HTTP::Lite: "  . $res;
+            return;
+        }
+    };
+
+}
+
+init();
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plient::Handler::HTTPLite - 
+
+
+=head1 SYNOPSIS
+
+    use Plient::Handler::HTTPLite;
+
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE
+
+=head1 AUTHOR
+
+sunnavy  C<< <sunnavy@bestpractical.com> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright 2010 Best Practical Solutions.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
---
+++ lib/Plient/Handler/LWP.pm
@@ -0,0 +1,71 @@
+package Plient::Handler::LWP;
+use strict;
+use warnings;
+
+my ( $LWP, %protocol, %method );
+sub method { $method{ $_[-1] } } # in case people call with ->
+
+
+sub init {
+    return unless $LWP && $LWP_config;
+    eval { require 'LWP::UserAgent' } or return;
+    
+    @protocol{HTTP} = ();
+
+    if ( eval { require 'Crypt::SSLeay' } ) {
+        @protocol{HTTPS} = ();
+    }
+
+    $method{http_get} = sub {
+        my ( $uri, $args ) = @_;
+
+        # XXX TODO tweak the new arguments
+        my $ua  = LWP::UserAgent->new;
+        my $res = $ua->get($uri);
+        if ( $res->is_success ) {
+            return $res->decoded_content;
+        }
+        else {
+            warn "failed to get $uri with lwp: " . $res->status_line;
+            return;
+        }
+    };
+
+    if ( exists $protocol{HTTPS} ) {
+        # have you seen https is available while http is not?
+        $method{https_get} = $method{http_get};
+    }
+}
+
+init();
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plient::Handler::LWP - 
+
+
+=head1 SYNOPSIS
+
+    use Plient::Handler::LWP;
+
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE
+
+=head1 AUTHOR
+
+sunnavy  C<< <sunnavy@bestpractical.com> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright 2010 Best Practical Solutions.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
