0559233a407bb4f92c1b4ef44d1bb77af1d9594e
sunnavy <sunnavy@bestpractical.com>
2010-04-15 06:02:49 +0800

    basic Plient.pm

--- lib/Plient.pm
+++ lib/Plient.pm
@@ -3,12 +3,79 @@ package Plient;
 use warnings;
 use strict;
 use Carp;
-
 our $VERSION = '0.01';
+use File::Spec::Functions;
+use base 'Exporter';
+our @EXPORT = 'plient';
 
+sub plient {
+    my ( $method, $uri, $args ) = @_;
+    if ( $args && ref $args ne 'HASH' ) {
+        warn 'invalid args: should be a hashref';
+        return;
+    }
+    $args ||= {};
+    $uri =~ s/^\s+//;
+    my $sub = dispatch( $method, $uri );
+    if ( $sub ) {
+        $sub->( $args );
+    }
+    else {
+        warn "failed to $method on $uri"; 
+        return;
+    }
+}
 
+my %dispatch_map = (
+    'file://'  => 'Plient::Protocol::File',
+    'http://'  => 'Plient::Protocol::HTTP',
+    'https://' => 'Plient::Protocol::HTTPS',
+);
 
+sub dispatch {
+    my ( $method, $uri ) = @_;
+    $method = lc $method;
+    $method ||= 'get';    # people use get most of the time.
 
+    for my $prefix ( keys %dispatch_map ) {
+        if ( $uri =~ m{^\Q$prefix} ) {
+            my $class = $dispatch_map{$prefix};
+            eval "require $class" or warn "failed to require $class" && return;
+            if ( my $sub = $class->can($method) || $class->method($method) ) {
+                return sub { $sub->( $uri, @_ ) };
+            }
+            else {
+                warn "unsupported $method";
+            }
+        }
+    }
+}
+
+my @handlers;
+sub handlers {
+    return @handlers if @handlers;
+    load_handlers();
+}
+
+sub load_handlers {
+    my @hd;
+    for my $inc (@INC) {
+        my $handler_dir = catdir( $inc, 'Plient', 'Handler' );
+        if ( -e $handler_dir ) {
+            if ( opendir my $dh, $handler_dir ) {
+                push @hd,
+                  map { /(\w+)\.pm/ ? "Plient::Handler::$1" : () } readdir $dh;
+            }
+            else {
+                warn "can't read $handler_dir";
+            }
+        }
+    }
+    for my $hd (@hd) {
+        eval "require $hd" or warn "failed to require $hd";
+    }
+    @handlers = @hd;
+}
 
 1;
 
