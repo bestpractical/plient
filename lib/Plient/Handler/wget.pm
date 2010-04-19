e5b146c5a063693e62a2a88a77cfc855ad40faa5
sunnavy <sunnavy@bestpractical.com>
2010-04-20 07:29:44 +0800

    basic post for wget

--- lib/Plient/Handler/wget.pm
+++ lib/Plient/Handler/wget.pm
@@ -22,7 +22,7 @@ sub init {
     $wget = $ENV{PLIENT_WGET} || which('wget');
     return unless $wget;
 
-    @protocol{qw/http https ftp/}     = ();
+    @protocol{qw/http https ftp/} = ();
 
     {
         local $ENV{LC_ALL} = 'en_US';
@@ -31,7 +31,7 @@ sub init {
             delete $protocol{https};
         }
     }
-    
+
     $method{http_get} = sub {
         my ( $uri, $args ) = @_;
         if ( open my $fh, "$wget -q -O - $uri |" ) {
@@ -44,6 +44,40 @@ sub init {
         }
     };
     $method{https_get} = $method{http_get} if exists $protocol{https};
+
+    $method{http_post} = sub {
+        my ( $uri, $args ) = @_;
+        $args ||= {};
+
+        my $data = '';
+        if ( $args->{body} ) {
+            my %kv = %{ $args->{body} };
+            for my $k ( keys %kv ) {
+                if ( defined $kv{$k} ) {
+                    if ( ref $kv{$k} && ref $kv{$k} eq 'ARRAY' ) {
+                        for my $i ( @{ $kv{$k} } ) {
+                            $data .= " --post-data $k=$i";
+                        }
+                    }
+                    else {
+                        $data .= " --post-data $k=$kv{$k}";
+                    }
+                }
+                else {
+                    $data .= " --post-data $k=";
+                }
+            }
+        }
+
+        if ( open my $fh, "$wget -q -O - $data $uri |" ) {
+            local $/;
+            <$fh>;
+        }
+        else {
+            warn "failed to post $uri with curl: $!";
+            return;
+        }
+    };
     return 1;
 }
 
