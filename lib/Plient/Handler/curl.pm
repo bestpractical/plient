ca4db3e34e4d4ad5272fd623e3c0fca8ddb7ea4a
sunnavy <sunnavy@gmail.com>
2011-11-12 05:55:57 +0800

    copyright update

--- lib/Plient/Handler/curl.pm
+++ lib/Plient/Handler/curl.pm
@@ -179,7 +179,7 @@ sunnavy  C<< <sunnavy@bestpractical.com> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright 2010 Best Practical Solutions.
+Copyright 2010-2011 Best Practical Solutions.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
