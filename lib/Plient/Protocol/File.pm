09439323f695b3b1b2c42146be33221811c31252
sunnavy <sunnavy@gmail.com>
2012-02-24 14:49:08 +0800

    update copyright to 2012!

--- lib/Plient/Protocol/File.pm
+++ lib/Plient/Protocol/File.pm
@@ -41,7 +41,7 @@ sunnavy  C<< <sunnavy@bestpractical.com> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright 2010-2011 Best Practical Solutions.
+Copyright 2010-2012 Best Practical Solutions.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
